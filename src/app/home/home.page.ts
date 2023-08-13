282fe58ec43976aca9f86120cfa2ae70e9dfd5f1
emmanuel-logmaster <emmanuel@logmaster.com.au>
2023-08-14 00:05:14 +0800

    fix: integrated mlkit translation capacitor plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2 } from '@angular/core';
-import { LoadingController, AlertController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
+import { Language, Translation } from '@capacitor-mlkit/translation';
 
 @Component({
   selector: 'app-home',
@@ -8,124 +9,66 @@ import { LoadingController, AlertController } from '@ionic/angular';
 })
 
 export class HomePage {
-  public text: string = '';
-  public originalWords: Array<string> = [];
-  public shuffledWords: Array<string> = [];
-  public shuffledButtons: Array<any> = [];
-  public correctButtons: Array<any> = [];
-  public currentOriginalWordIndex: number = 0;
-  public shuffledButtonsSectionHeight: number = 0;
-  @ViewChild('shuffledButtonsSection', { static: true }) shuffledButtonsSection!: ElementRef;
+  public readonly language = Language;
+  public translateFormGroup = new UntypedFormGroup({
+    text: new UntypedFormControl(''),
+    sourceLanguage: new UntypedFormControl(Language.English),
+    targetLanguage: new UntypedFormControl(Language.German),
+    translatedText: new UntypedFormControl(''),
+  });
+  public manageModelsFormGroup = new UntypedFormGroup({
+    languages: new UntypedFormControl([]),
+  });
+  public disableSaveModelsButton = false;
 
-  constructor(
-    private loadingController: LoadingController,
-    private alertController: AlertController
-  ) {}
+  private readonly githubUrl = 'https://github.com/robingenz/capacitor-mlkit';
 
-  ngOnInit() {
-    this.load();
+  constructor() {}
+
+  public ngOnInit(): void {
+    this.getDownloadedModels();
   }
 
-  ngAfterViewInit() {
-    this.observeButtonsSectionHeightChanges();
+  public openOnGithub(): void {
+    window.open(this.githubUrl, '_blank');
   }
 
-  public load(): void {
-    this.text = 'This is a long form text here';
-    this.originalWords = this.separateWords(this.text);
-    this.shuffledWords = this.shuffleWords(this.originalWords);
-    this.shuffledButtons = this.setButtonsData(this.shuffledWords);
-    this.correctButtons = [];
-    this.currentOriginalWordIndex = 0;
-    console.log(this.text);
-    console.log(this.originalWords);
-    console.log(this.shuffledWords);
-    console.log(this.shuffledButtons);
-  }
-  
-  public separateWords(text: string): Array<string> {
-    return text.split(' ');
-  }
-
-  public shuffleWords(words: Array<string>): Array<string> {
-    const shuffledWords = [...words];
-    for (let i = shuffledWords.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledWords[i], shuffledWords[j]] = [shuffledWords[j], shuffledWords[i]];
+  public async saveModels(): Promise<void> {
+    this.disableSaveModelsButton = true;
+    const languages: Language[] =
+      this.manageModelsFormGroup.get('languages')?.value;
+    if (!languages) {
+      return;
     }
-    return shuffledWords;
-  }
-
-  public setButtonsData(shuffledWords: Array<any>): Array<any> {
-    const buttons: Array<any> = [];
-    shuffledWords.map((word: string, index: number) => {
-      const originalWordIndex = this.originalWords.indexOf(word);
-      buttons[index] = {
-        index: originalWordIndex,
-        word: word,
-        color: 'dark',
-        isDisabled: false,
-        isDisabledPermanently: false
+    for (const availableLanguage of Object.values(Language)) {
+      if (languages.includes(availableLanguage)) {
+        await Translation.downloadModel({ language: availableLanguage });
+      } else {
+        await Translation.deleteDownloadedModel({
+          language: availableLanguage,
+        });
       }
-    });
-    return buttons;
-  }
-
-  public async onTapButton(button: any) {
-    this.enableDisableButtons();
-    const buttonIndex = this.shuffledButtons.indexOf(button);
-    if (this.currentOriginalWordIndex === button.index) {
-      this.shuffledButtons[buttonIndex].color = 'success';
-      this.shuffledButtons[buttonIndex].isDisabledPermanently = true;
-      this.currentOriginalWordIndex++;
-      setTimeout(async () => {
-        this.correctButtons.push(button);
-        this.enableDisableButtons();
-        await this.showAllDoneAlert();
-        this.shuffledButtons[buttonIndex].color = 'medium';
-      }, 300)
-    } else {
-      this.enableDisableButtons();
-      this.shuffledButtons[buttonIndex].color = 'danger';
-      setTimeout(async () => {
-        this.shuffledButtons[buttonIndex].color = 'dark';
-      }, 300)
     }
+    this.disableSaveModelsButton = false;
   }
 
-  enableDisableButtons() {
-    this.shuffledButtons = this.shuffledButtons.map((shuffledButton) => {
-      shuffledButton.isDisabled = !shuffledButton.isDisabled;
-      return shuffledButton;
-    });
+  public async getDownloadedModels(): Promise<void> {
+    const { languages } = await Translation.getDownloadedModels();
+    this.manageModelsFormGroup.patchValue({ languages });
   }
 
-  async showAllDoneAlert() {
-    if (this.currentOriginalWordIndex === this.originalWords.length) {
-      const alert = await this.alertController.create({
-        message: 'All done',
-        buttons: ['OK'],
-        backdropDismiss: false
-      });
-  
-      await alert.present();
+  public async translate(): Promise<void> {
+    const text = this.translateFormGroup.get('text')?.value;
+    const sourceLanguage = this.translateFormGroup.get('sourceLanguage')?.value;
+    const targetLanguage = this.translateFormGroup.get('targetLanguage')?.value;
+    if (!text || !sourceLanguage || !targetLanguage) {
+      return;
     }
-  }
-
-  reset() {
-    this.load();
-  }
-
-  private observeButtonsSectionHeightChanges() {
-    const targetElement = this.shuffledButtonsSection.nativeElement;
-    const resizeObserver = new ResizeObserver(entries => {
-      for (const entry of entries) {
-        if (this.shuffledButtonsSectionHeight === 0) {
-        this.shuffledButtonsSectionHeight = entry.contentRect.height;
-        }
-        console.log('Div height changed:', this.shuffledButtonsSectionHeight);
-      }
+    const result = await Translation.translate({
+      text,
+      sourceLanguage,
+      targetLanguage,
     });
-    resizeObserver.observe(targetElement);
+    this.translateFormGroup.patchValue({ translatedText: result.text });
   }
 }
