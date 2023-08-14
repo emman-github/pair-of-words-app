6cbeb3b2654b10b1bcdfb8225ca097eb1d98e38b
emmanuel-logmaster <emmanuel@logmaster.com.au>
2023-08-14 14:25:51 +0800

    feat: ui for pair words app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,66 +9,98 @@ import { Language, Translation } from '@capacitor-mlkit/translation';
 })
 
 export class HomePage {
-  public readonly language = Language;
-  public translateFormGroup = new UntypedFormGroup({
-    text: new UntypedFormControl(''),
-    sourceLanguage: new UntypedFormControl(Language.English),
-    targetLanguage: new UntypedFormControl(Language.German),
-    translatedText: new UntypedFormControl(''),
-  });
-  public manageModelsFormGroup = new UntypedFormGroup({
-    languages: new UntypedFormControl([]),
-  });
-  public disableSaveModelsButton = false;
+  public englishText: string = '';
+  public pairOfWords: Array<any> = [];
+  // public readonly language = Language;
+  // public translateFormGroup = new UntypedFormGroup({
+  //   text: new UntypedFormControl(''),
+  //   sourceLanguage: new UntypedFormControl(Language.English),
+  //   targetLanguage: new UntypedFormControl(Language.German),
+  //   translatedText: new UntypedFormControl(''),
+  // });
+  // public manageModelsFormGroup = new UntypedFormGroup({
+  //   languages: new UntypedFormControl([]),
+  // });
+  // public disableSaveModelsButton = false;
 
-  private readonly githubUrl = 'https://github.com/robingenz/capacitor-mlkit';
+  // private readonly githubUrl = 'https://github.com/robingenz/capacitor-mlkit';
 
-  constructor() {}
+  // constructor() {}
 
-  public ngOnInit(): void {
-    this.getDownloadedModels();
-  }
+  // public ngOnInit(): void {
+  //   this.getDownloadedModels();
+  // }
 
-  public openOnGithub(): void {
-    window.open(this.githubUrl, '_blank');
-  }
+  // public openOnGithub(): void {
+  //   window.open(this.githubUrl, '_blank');
+  // }
 
-  public async saveModels(): Promise<void> {
-    this.disableSaveModelsButton = true;
-    const languages: Language[] =
-      this.manageModelsFormGroup.get('languages')?.value;
-    if (!languages) {
-      return;
-    }
-    for (const availableLanguage of Object.values(Language)) {
-      if (languages.includes(availableLanguage)) {
-        await Translation.downloadModel({ language: availableLanguage });
-      } else {
-        await Translation.deleteDownloadedModel({
-          language: availableLanguage,
-        });
+  // public async saveModels(): Promise<void> {
+  //   this.disableSaveModelsButton = true;
+  //   const languages: Language[] =
+  //     this.manageModelsFormGroup.get('languages')?.value;
+  //   if (!languages) {
+  //     return;
+  //   }
+  //   for (const availableLanguage of Object.values(Language)) {
+  //     if (languages.includes(availableLanguage)) {
+  //       await Translation.downloadModel({ language: availableLanguage });
+  //     } else {
+  //       await Translation.deleteDownloadedModel({
+  //         language: availableLanguage,
+  //       });
+  //     }
+  //   }
+  //   this.disableSaveModelsButton = false;
+  // }
+
+  // public async getDownloadedModels(): Promise<void> {
+  //   const { languages } = await Translation.getDownloadedModels();
+  //   this.manageModelsFormGroup.patchValue({ languages });
+  // }
+
+  // public async translate(): Promise<void> {
+  //   const text = this.translateFormGroup.get('text')?.value;
+  //   const sourceLanguage = this.translateFormGroup.get('sourceLanguage')?.value;
+  //   const targetLanguage = this.translateFormGroup.get('targetLanguage')?.value;
+  //   if (!text || !sourceLanguage || !targetLanguage) {
+  //     return;
+  //   }
+  //   const result = await Translation.translate({
+  //     text,
+  //     sourceLanguage,
+  //     targetLanguage,
+  //   });
+  //   this.translateFormGroup.patchValue({ translatedText: result.text });
+  // }
+
+  constructor() {
+    this.englishText = 'horse,house,moon,landscape,car,woman';
+    this.pairOfWords = [
+      {
+        english: 'horse',
+        translated: 'auto'
+      },
+      {
+        english: 'house',
+        translated: 'frau'
+      },
+      {
+        english: 'moon',
+        translated: 'landschaft'
+      },
+      {
+        english: 'landscape',
+        translated: 'mond'
+      },
+      {
+        english: 'car',
+        translated: 'pferd'
+      },
+      {
+        english: 'woman',
+        translated: 'haus'
       }
-    }
-    this.disableSaveModelsButton = false;
-  }
-
-  public async getDownloadedModels(): Promise<void> {
-    const { languages } = await Translation.getDownloadedModels();
-    this.manageModelsFormGroup.patchValue({ languages });
-  }
-
-  public async translate(): Promise<void> {
-    const text = this.translateFormGroup.get('text')?.value;
-    const sourceLanguage = this.translateFormGroup.get('sourceLanguage')?.value;
-    const targetLanguage = this.translateFormGroup.get('targetLanguage')?.value;
-    if (!text || !sourceLanguage || !targetLanguage) {
-      return;
-    }
-    const result = await Translation.translate({
-      text,
-      sourceLanguage,
-      targetLanguage,
-    });
-    this.translateFormGroup.patchValue({ translatedText: result.text });
+    ]
   }
 }
